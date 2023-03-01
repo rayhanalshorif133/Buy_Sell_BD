282c9dfae21f3010148627850d94d30d4ed41283
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-03-01 17:13:46 +0600

    color change in dark mode

--- resources/views/home/_partials/what-we-do.blade.php
+++ resources/views/home/_partials/what-we-do.blade.php
@@ -13,55 +13,7 @@
                         <img class="img-fluid w-100 what_we_do_image"
                             src="{{asset('web/assets/images/what_we_do/membership.jpg')}}" alt="membership"
                             loading="lazy"></div>
-                    <div class="card_text what_we_do_text">
-                        <p>
-                            We provide an easy and convenient way to transfer your club membership
-                        </p>
-                    </div>
-                </div>
-            </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-sm-6 pb-6 course-padding-area sal-animate" data-sal="slide-right"
-            data-sal-duration="800" data-sal-easing="ease">
-            <a href="#">
-                <div class="course_single_item what_we_do">
-                    <div class="card_image">
-                        <img class="img-fluid w-100 what_we_do_image"
-                            src="{{asset('web/assets/images/what_we_do/membership.jpg')}}" alt="membership"
-                            loading="lazy"></div>
-                    <div class="card_text what_we_do_text">
-                        <p>
-                            We provide an easy and convenient way to transfer your club membership
-                        </p>
-                    </div>
-                </div>
-            </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-sm-6 pb-6 course-padding-area sal-animate" data-sal="slide-right"
-            data-sal-duration="800" data-sal-easing="ease">
-            <a href="#">
-                <div class="course_single_item what_we_do">
-                    <div class="card_image">
-                        <img class="img-fluid w-100 what_we_do_image"
-                            src="{{asset('web/assets/images/what_we_do/membership.jpg')}}" alt="membership"
-                            loading="lazy"></div>
-                    <div class="card_text what_we_do_text">
-                        <p>
-                            We provide an easy and convenient way to transfer your club membership
-                        </p>
-                    </div>
-                </div>
-            </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-sm-6 pb-6 course-padding-area sal-animate" data-sal="slide-right"
-            data-sal-duration="800" data-sal-easing="ease">
-            <a href="#">
-                <div class="course_single_item what_we_do">
-                    <div class="card_image">
-                        <img class="img-fluid w-100 what_we_do_image"
-                            src="{{asset('web/assets/images/what_we_do/membership.jpg')}}" alt="membership"
-                            loading="lazy"></div>
-                    <div class="card_text what_we_do_text">
+                    <div class="what_we_do_text">
                         <p>
                             We provide an easy and convenient way to transfer your club membership
                         </p>
