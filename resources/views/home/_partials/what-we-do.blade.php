265f494fffa7b1effec7b962c6222d44f4276949
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-03-02 11:59:05 +0600

    what we do

--- resources/views/home/_partials/what-we-do.blade.php
+++ resources/views/home/_partials/what-we-do.blade.php
@@ -7,25 +7,52 @@
         </div>
         <div class="col-lg-3 col-md-4 col-sm-6 pb-6 course-padding-area sal-animate" data-sal="slide-right"
             data-sal-duration="800" data-sal-easing="ease">
-            <a href="#">
-                <div class="course_single_item what_we_do">
-                    <div class="card_image">
-                        <img class="img-fluid w-100 what_we_do_image"
-                            src="{{asset('web/assets/images/what_we_do/membership.jpg')}}" alt="membership"
-                            loading="lazy">
-                    </div>
-                    <div class="subtitle p-3">
-                        <h4>
-                           Club Membership
-                        </h4>
-                        <p>We offer club memberships, including both online and offline services. Some popular online providers include ClassPass,
+            <div class="course_single_item what_we_do">
+                <div class="card_image">
+                    <img class="img-fluid w-100 what_we_do_image"
+                        src="{{asset('web/assets/images/what_we_do/membership.jpg')}}" alt="membership"
+                        loading="lazy">
+                </div>
+                <div class="subtitle p-3">
+                    <h4>
+                       Club Membership
+                    </h4>
+                    <p class="less">
+                    We offer club memberships, including both online and offline services. Some popular online providers include ClassPass,
+                    which offers memberships to classes and
+                    </p>
+                    <p class="d-none more">
+                        We offer club memberships, including both online and offline services. Some popular online providers include ClassPass,
                         which offers memberships to classes and activities at local gyms, studios, and other venues, and Meetup, which helps
                         people find and join local groups with similar interests. Offline providers may include local organizations such as
-                        country clubs, sports teams, and community groups.</p>
+                        country clubs, sports teams, and community groups.
+                    </p>
+                    <div class="text-left read_more_what_we_do">
+                        <span class="btn btn-md btn-buy-sell">Read More</span>
                     </div>
+                    <div class="read_less_what_we_do d-none text-left">
+                        <span class="btn btn-md btn-buy-sell-danger">Read Less</span>
+                    </div>
+
                 </div>
-            </a>
+            </div>
         </div>
     </div>
     <span id="riz_event" class="mb-4"></span>
 </div>
+<script>
+    $(function(){
+        $(".read_more_what_we_do").on("click", function(){
+            $(this).toggleClass("d-none");
+            $(this).parent().find(".more").toggleClass("d-none");
+            $(this).parent().find(".less").toggleClass("d-none");
+            $(this).parent().parent().find(".read_less_what_we_do").toggleClass("d-none");
+        });
+        $(".read_less_what_we_do").on("click", function(){
+            $(this).toggleClass("d-none");
+            $(this).parent().find(".more").toggleClass("d-none");
+            $(this).parent().find(".less").toggleClass("d-none");
+            $(this).parent().parent().find(".read_more_what_we_do").toggleClass("d-none");
+        });
+    });
+</script>
