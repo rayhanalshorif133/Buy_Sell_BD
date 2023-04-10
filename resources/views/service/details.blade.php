6160f63a3907e53ce839ba73ad5bd4cf6023b213
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-04-10 15:00:41 +0600

    details view

--- resources/views/service/details.blade.php
+++ resources/views/service/details.blade.php
@@ -7,9 +7,8 @@
 @endsection
 
 @section('content')
-<div class="container">
-    <div class="col-md-12">
-        <div class="card">
+<div class="col-md-12">
+    <div class="card">
             <div class="card-header p-2">
                 <h3 class="card-title text-bold">
                     Service Details
@@ -24,8 +23,69 @@
                 </div>
             </div>
             <div class="card-body">
+                <table class="table table-striped projects">
+                    <thead>
+                        <tr>
+                            <th style="width: 5%">
+                                #
+                            </th>
+                            <th style="width: 10%">
+                                Image
+                            </th>
+                            <th style="width: 10%">
+                                Title
+                            </th>
+                            <th style="width: 45%">
+                                Description
+                            </th>
+                            <th style="width: 10%" class="text-center">
+                                Actions
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if (count($serviceDetails) == 0)
+                        <tr>
+                            <td colspan="5" class="text-center">No data found</td>
+                        </tr>
+                        @endif
+                        @foreach ($serviceDetails as $service)
+                        <tr id="{{$service->id}}">
+                            <td>{{$loop->index+1}}</td>
+                            <td>
+                                <img src="{{asset($service->image)}}" alt="" width="100px">
+                            </td>
+                            <td>
+                                {{$service->title}}
+                            </td>
+                            <td>
+                                {{-- show 150 word --}}
+                                <span class="descriptionLess">{!! Str::limit($service->info,150) !!}</span>
+                                {{-- <span class="d-none getDescription">{!! $service->info !!}</span> --}}
+                                @if (strlen($service->description) > 150)
+                                <span style="color: blue" class="readMore btn">Read More</span>
+                                <span style="color: red" class="readLess btn d-none">Read Less</span>
+                                @endif
+                            </td>
+                            <td class="w-20">
+                                <a href="{{route('user.service.detailsView',$service->id)}}">
+                                    <span class="btn btn-success btn-sm" title="View">
+                                        <i class="fas fa-eye"></i>
+                                    </span>
+                                </a>
+                                <span class="btn btn-info btn-sm editBtn" data-toggle="modal"
+                                    data-target="#modal-add-and-update-service">
+                                    <i class="fas fa-pencil-alt"></i>
+                                </span>
+                                <span class="btn btn-danger btn-sm deleteBtn">
+                                    <i class="fas fa-trash"></i>
+                                </span>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
-        </div>
     </div>
 </div>
 <div class="modal fade" id="addNewItem" aria-hidden="true" style="display: none;">
